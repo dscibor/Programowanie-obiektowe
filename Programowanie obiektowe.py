60fc0e63c48f72a5164052561ba9f9a93f59c10f
dscibor <damianscibor13@gmail.com>
2021-02-08 22:57:51 +0100

    Poprawa estetyki

--- Programowanie obiektowe.py
+++ Programowanie obiektowe.py
@@ -1,5 +1,5 @@
 
-filename = "filename.txt"
+filename = "MojaFarma.txt"
 
 class Zwierze:
     def __init__(self, gatunek = "", imie = "", wiek = 0):
@@ -21,19 +21,26 @@ class Zwierze:
 
 def get_zwierzaki_from_file(filename):
     zwierzaki = []
-    with open("filename.txt", 'r') as file:
-        for line in file:
-            l = line.rstrip('\n')
-            lista_slow = l.split(' ') 
-            gatunek = lista_slow[0]
-            imie = lista_slow[1]
-            wiek = lista_slow[2]
-            zwierzak = Zwierze(gatunek, imie, wiek)
-            zwierzaki.append(zwierzak)
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                l = line.rstrip('\n')
+                lista_slow = l.split(' ') 
+                gatunek = lista_slow[0]
+                imie = lista_slow[1]
+                wiek = lista_slow[2]
+                zwierzak = Zwierze(gatunek, imie, wiek)
+                zwierzaki.append(zwierzak)
+    except:
+        print("Nie ma takiego pliku!")
     return zwierzaki
 
+def clear_lista(filename):
+    with open(filename, 'w') as file:
+        pass
+
 def save_zwierzaki_to_file(filename, zwierzaki):
-    with open("filename.txt", 'w') as file:
+    with open(filename, 'w') as file:
         for zwierzak in zwierzaki:
             file.write('{} {} {}\n'.format(zwierzak.gatunek, zwierzak.imie, zwierzak.wiek))
 
@@ -47,13 +54,18 @@ print("Dodaj zwierzę")
 gatunek = input("Podaj gatunek zwierzęcia: ")
 imie = input("Podaj imię zwierzęcia: ")
 wiek = input("Podaj wiek zwierzęcia: ")
-zwierzak = Zwierze(gatunek, imie ,wiek)
-zwierzaki.append(zwierzak)
-print(zwierzak.gatunek, zwierzak.imie, zwierzak.wiek)
 
-save_zwierzaki_to_file(filename, zwierzaki)
+if gatunek == "clear" or imie == "clear" or wiek == "clear":
+    clear_lista(filename)
+    print("Wyczyszczono farmę!")
+else:
+    zwierzak = Zwierze(gatunek, imie ,wiek)
+    zwierzaki.append(zwierzak)
+    print(zwierzak.gatunek, zwierzak.imie, zwierzak.wiek)
 
+    save_zwierzaki_to_file(filename, zwierzaki)
     
+
 
 
 
